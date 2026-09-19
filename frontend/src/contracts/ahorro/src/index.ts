579fad8ca6becca2fe00c5d77ef713e8b2f35803
cristinanataliaschwindt-create <cristinanataliaschwindt@gmail.com>
2026-09-19 15:13:01 -0300

    Contrato

---
+++ frontend/src/contracts/ahorro/src/index.ts
@@ -0,0 +1,80 @@
+import { Buffer } from "buffer";
+import { Address } from "@stellar/stellar-sdk";
+import {
+  AssembledTransaction,
+  Client as ContractClient,
+  ClientOptions as ContractClientOptions,
+  MethodOptions,
+  Result,
+  Spec as ContractSpec,
+} from "@stellar/stellar-sdk/contract";
+import type {
+  u32,
+  i32,
+  u64,
+  i64,
+  u128,
+  i128,
+  u256,
+  i256,
+  Option,
+  Timepoint,
+  Duration,
+} from "@stellar/stellar-sdk/contract";
+export * from "@stellar/stellar-sdk";
+export * as contract from "@stellar/stellar-sdk/contract";
+export * as rpc from "@stellar/stellar-sdk/rpc";
+
+if (typeof window !== "undefined") {
+  //@ts-ignore Buffer exists
+  window.Buffer = window.Buffer || Buffer;
+}
+
+
+export const networks = {
+  testnet: {
+    networkPassphrase: "Test SDF Network ; September 2015",
+    contractId: "CB7WHVS6LV65H7V4LOOL7P27DUH7GNOZIZFD33Q5TOPOHCV64ZBI5KTG",
+  }
+} as const
+
+
+export interface Client {
+  /**
+   * Construct and simulate a consultar_saldo transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  consultar_saldo: ({usuario}: {usuario: string}, options?: MethodOptions) => Promise<AssembledTransaction<u64>>
+
+  /**
+   * Construct and simulate a registrar_ahorro transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  registrar_ahorro: ({usuario, monto}: {usuario: string, monto: u64}, options?: MethodOptions) => Promise<AssembledTransaction<null>>
+
+}
+export class Client extends ContractClient {
+  static async deploy<T = Client>(
+    /** Options for initializing a Client as well as for calling a method, with extras specific to deploying. */
+    options: MethodOptions &
+      Omit<ContractClientOptions, "contractId"> & {
+        /** The hash of the Wasm blob, which must already be installed on-chain. */
+        wasmHash: Buffer | string;
+        /** Salt used to generate the contract's ID. Passed through to {@link Operation.createCustomContract}. Default: random. */
+        salt?: Buffer | Uint8Array;
+        /** The format used to decode `wasmHash`, if it's provided as a string. */
+        format?: "hex" | "base64";
+      }
+  ): Promise<AssembledTransaction<T>> {
+    return ContractClient.deploy(null, options)
+  }
+  constructor(public readonly options: ContractClientOptions) {
+    super(
+      new ContractSpec([ "AAAAAAAAAAAAAAAPY29uc3VsdGFyX3NhbGRvAAAAAAEAAAAAAAAAB3VzdWFyaW8AAAAAEwAAAAEAAAAG",
+        "AAAAAAAAAAAAAAAQcmVnaXN0cmFyX2Fob3JybwAAAAIAAAAAAAAAB3VzdWFyaW8AAAAAEwAAAAAAAAAFbW9udG8AAAAAAAAGAAAAAA==" ]),
+      options
+    )
+  }
+  public readonly fromJSON = {
+    consultar_saldo: this.txFromJSON<u64>,
+        registrar_ahorro: this.txFromJSON<null>
+  }
+}
